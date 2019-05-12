9f434ce64db564f3768f4f4a695ccf5b92f5b865
Juke-99 <yanpahika@gmail.com>
2019-05-12 22:33:32 +0900

    create form

---
+++ components/FormSample.js
@@ -0,0 +1,30 @@
+class FormSample extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value})
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>Name: <input type="text" vslue={this.state.value} onChange={this.handleChange}></input></label>
+        <input type="submit" value="submit"></input>
+      </form>
+    )
+  }
+}
+
+export default FormSample
---
+++ pages/formSample.js
@@ -0,0 +1,7 @@
+import NameChangeForm from '../components/FormSample'
+
+export default function formSample() {
+  return (
+    <NameChangeForm></NameChangeForm>
+  )
+}
